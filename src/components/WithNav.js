7b9db10bcf5fec3c6406773bc727e205ba31c6d1
vishnuhaasan <vishnuhaasanoffl@gmail.com>
2022-03-01 19:35:05 +0530

    v1:first

--- src/components/WithNav.js
+++ src/components/WithNav.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 
-export default WithNav = () => {
+export default () => {
     return(
         <>
         <Navbar />
--- src/components/WithoutNav.js
+++ src/components/WithoutNav.js
@@ -1,4 +1,4 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
 
-export default WithoutNav = () => <Outlet />
+export default () => <Outlet />
